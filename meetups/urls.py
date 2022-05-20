a8c4f054061272a3901b0db2c211d07f062708f8
sershishkov <ser.shishkov@gmail.com>
2022-05-20 11:22:12 +0300

    done

---
+++ meetups/urls.py
@@ -0,0 +1,11 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="all-meetups"),
+    path("<slug:meetup_slug>/success", views.confirm_registration,
+         name="confirm-registration"),
+    path("<slug:meetup_slug>",
+         views.meetup_details, name="meetup-detail"),
+]
